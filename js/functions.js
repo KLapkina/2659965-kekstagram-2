a8f9f7fc2d448a361b7f3868ff44e874e02a97dd
KLapkina <lapkinaaksenia@gmail.com>
2026-05-23 19:50:37 +0300

    правки + настройки форматтеров

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,15 @@
 const parseTime = (stringTime) => stringTime.split(':').map(Number);
-const timeToMinutes = function (stringTime) {
+const timeToMinutes = (stringTime) => {
   const parsedString = parseTime(stringTime);
   return parsedString[0] * 60 + parsedString[1];
 };
 
-const isMeetingWithInWorkday = function(beginWorkDay, endWorkDay, beginMeeting, countMinutes) {
+const isMeetingWithInWorkday = (
+  beginWorkDay,
+  endWorkDay,
+  beginMeeting,
+  countMinutes,
+) => {
   const workBegin = timeToMinutes(beginWorkDay);
   const workEnd = timeToMinutes(endWorkDay);
   const meetBegin = timeToMinutes(beginMeeting);
@@ -15,37 +20,4 @@ const isMeetingWithInWorkday = function(beginWorkDay, endWorkDay, beginMeeting, 
   return false;
 };
 
-export {isMeetingWithInWorkday};
-
-
-// const convertToDate = function (stringTime) {
-//   const date = new Date();
-//   date.setHours(parseTime(stringTime)[0]);
-//   date.setMinutes(parseTime(stringTime)[1]);
-//   return date;
-// };
-// const createTimeObject = function (beginTime, endTime) {
-//   return {
-//     begin: convertToDate(beginTime),
-//     end: convertToDate(endTime)
-//   };
-// };
-// const calculateEndMeeting = function (beginMeeting, countMinutes) {
-//   const beginMeetingMinutes = Number(parseTime(beginMeeting)[0]) * 60 + Number(parseTime(beginMeeting)[1]);
-//   const allMinutes = beginMeetingMinutes + Number(countMinutes);
-//   const hours = Math.floor(allMinutes / 60);
-//   const minutes = allMinutes % 60;
-//   return `${hours}:${minutes}`;
-// };
-// const isMeetingWithInWorkday = function(beginWorkDay, endWorkDay, beginMeeting, countMinutes) {
-//   const endMeeting = calculateEndMeeting(beginMeeting, countMinutes);
-//   const timeWorkDay = createTimeObject(beginWorkDay, endWorkDay);
-//   const timeMeeting = createTimeObject(beginMeeting, endMeeting);
-//   if (timeMeeting.begin >= timeWorkDay.begin && timeMeeting.end <= timeWorkDay.end) {
-//     return true;
-//   }
-//   return false;
-// };
-// export {isMeetingWithInWorkday};
-
-
+export { isMeetingWithInWorkday };
--- js/generate-objects.js
+++ js/generate-objects.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-console */
 import { MESSAGES_ARRAY, DESCRIPTION_ARRAY, NAMES } from './data.js';
-import { getRandomArrayElement, createRandomRangeInteger, createIdGenerator } from './util.js';
+import {
+  getRandomArrayElement,
+  createRandomRangeInteger,
+  createIdGenerator,
+} from './util.js';
 const NumberOfAvatars = {
   MIN: 1,
-  MAX: 6
+  MAX: 6,
 };
 const NumberOfLikes = {
   MIN: 15,
-  MAX: 200
+  MAX: 200,
 };
 const NumberOfComments = {
   MIN: 0,
-  MAX: 30
+  MAX: 30,
 };
-
 
 const generateCommentId = createIdGenerator(1, 100000);
 const generatePhotoId = createIdGenerator(1, 25);
@@ -25,37 +28,36 @@ const createMessage = () => {
     return first;
   }
   const second = getRandomArrayElement(MESSAGES_ARRAY);
-  return `${first } ${ second}`;
+  return `${first} ${second}`;
 };
 
 const createComment = () => ({
   id: generateCommentId(), //случайное число, не должны повторяться
-  avatar: `img/avatar-${ createRandomRangeInteger(NumberOfAvatars.MIN,NumberOfAvatars.MAX) }.svg`,//число от 1 до 6 выбирается рандомно
+  avatar: `img/avatar-${createRandomRangeInteger(NumberOfAvatars.MIN, NumberOfAvatars.MAX)}.svg`, //число от 1 до 6 выбирается рандомно
   message: createMessage(),
   name: getRandomArrayElement(NAMES), //массив имён, выбирается рандомно
 });
 
-const createCommentsArray = function () {
+const createCommentsArray = () => {
   const commentsArray = [];
-  const count = createRandomRangeInteger (NumberOfComments.MIN, NumberOfComments.MAX);
+  const count = createRandomRangeInteger(
+    NumberOfComments.MIN,
+    NumberOfComments.MAX,
+  );
   for (let i = 0; i <= count; i++) {
     commentsArray[i] = createComment();
   }
   return commentsArray;
 };
 
-const createDescriptionPhoto = function () {
-  return {
-    id: generatePhotoId(),//уникальное число от 1 до 25 - целое???
-    url: `photos/${ generatePhotoNumber() }.jpg`, //адрес картинки photos/{{i}}.jpg, i - случаное и не должно повторяться
-    description: getRandomArrayElement(DESCRIPTION_ARRAY), //?????
-    likes: createRandomRangeInteger(NumberOfLikes.MIN, NumberOfLikes.MAX), //число лайков, поставленных фотографии, от 15 до 200
-    comments: createCommentsArray()//массив объектов комментарий, отсавленных к конкретной фотографии, случайное количество от 0 до 30
+const createDescriptionPhoto = () => ({
+  id: generatePhotoId(), //уникальное число от 1 до 25 - целое???
+  url: `photos/${generatePhotoNumber()}.jpg`, //адрес картинки photos/{{i}}.jpg, i - случаное и не должно повторяться
+  description: getRandomArrayElement(DESCRIPTION_ARRAY), //?????
+  likes: createRandomRangeInteger(NumberOfLikes.MIN, NumberOfLikes.MAX), //число лайков, поставленных фотографии, от 15 до 200
+  comments: createCommentsArray(), //массив объектов комментарий, отсавленных к конкретной фотографии, случайное количество от 0 до 30
+});
 
-  };
-};
-
-const createDescriptionsPhotoArray = function () {
-  return Array.from ({length: 25}, createDescriptionPhoto);
-};
-export {createDescriptionsPhotoArray};
+const createDescriptionsPhotoArray = () =>
+  Array.from({ length: 25 }, createDescriptionPhoto);
+export { createDescriptionsPhotoArray };
